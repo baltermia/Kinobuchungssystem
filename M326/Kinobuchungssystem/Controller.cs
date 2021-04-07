05a1739a120a2a4060d62e4ca2ee7509e07ea5ac
speyck <54800864+speyck@users.noreply.github.com>
2021-04-07 08:07:54 +0200

    feat: Added Newtonsoft.Json lib and all lists for the Controller class

--- M326/Kinobuchungssystem/Controller.cs
+++ M326/Kinobuchungssystem/Controller.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,14 +9,27 @@ namespace Kinobuchungssystem
 {
     class Controller
     {
+        [JsonIgnore]
         public IEnumerable<Cinema> Cinemas => _cinemas.Select(c => c.Value);
 
+        [JsonIgnore]
+        public IEnumerable<Room> Rooms=> _rooms.Select(r => r.Value);
+        [JsonIgnore]
         public IEnumerable<Customer> Customers => _customers.Select(c => c.Value);
 
+        [JsonIgnore]
         public IEnumerable<Movie> Movies => _movies.Select(m => m.Value);
 
+        [JsonIgnore]
         public IEnumerable<Booking> Bookings => _bookings.Select(b => b.Value);
 
+        [JsonIgnore]
+        public IEnumerable<Show> Shows => _shows.Select(s => s.Value);
+
+        private readonly Dictionary<int, Show> _shows;
+
+        private readonly Dictionary<int, Room> _rooms;
+        
         private readonly Dictionary<int, Cinema> _cinemas;
 
         private readonly Dictionary<int, Customer> _customers;
@@ -26,10 +40,22 @@ namespace Kinobuchungssystem
 
         public Controller()
         {
+            _shows = new Dictionary<int, Show>();
+            _rooms = new Dictionary<int, Room>();
             _cinemas = new Dictionary<int, Cinema>();
             _customers = new Dictionary<int, Customer>();
             _movies = new Dictionary<int, Movie>();
             _bookings = new Dictionary<int, Booking>();
+        }
+
+        public void AddShow(Show show)
+        {
+            _shows.Add(_shows.Count, show);
+        }
+
+        public void AddRoom(Room room)
+        {
+            _rooms.Add(_rooms.Count, room);
         }
 
         public void AddCinema(Cinema cinema)
@@ -50,6 +76,16 @@ namespace Kinobuchungssystem
         public void AddBookings(Booking booking)
         {
             _bookings.Add(_bookings.Count, booking);
+        }
+
+        public void GetDataFromFile(string path)
+        {
+
+        }
+
+        public void WriteDataIntoFile(string path)
+        {
+
         }
     }
 }
