5a63b01c05d09d331b27a39d3b2b08f197a28efa
speyck <54800864+speyck@users.noreply.github.com>
2021-04-15 08:13:13 +0200

    feat: Remove() and comments in SimpleCollection class

--- M326/Kinobuchungssystem/SimpleCollection.cs
+++ M326/Kinobuchungssystem/SimpleCollection.cs
@@ -7,27 +7,80 @@ namespace Kinobuchungssystem
 {
     public class SimpleCollection<T> : IEnumerable<T>
     {
+        /// <summary>
+        /// Holds all items in the collection
+        /// </summary>
         [JsonIgnore]
         public IEnumerable<T> Items => _items.Select(i => i.Value);
 
         [JsonProperty]
         private readonly Dictionary<int, T> _items;
 
+        /// <summary>
+        /// Creates a new SimpleCollection object
+        /// </summary>
+        /// <param name="items"></param>
         public SimpleCollection(Dictionary<int, T> items = null)
         {
             _items = items ?? new Dictionary<int, T>();
         }
 
+        /// <summary>
+        /// Creates a new SimpleCollection object
+        /// </summary>
+        /// <param name="items"></param>
         public SimpleCollection(IEnumerable<T> items)
         {
             _items = new Dictionary<int, T>();
             items?.ToList().ForEach(Add);
         }
 
+        /// <summary>
+        /// Adds the item to the collection
+        /// </summary>
+        /// <param name="item"></param>
         public void Add(T item)
         {
-            _items.Add(_items.Count, item);
+            _items.Add(Enumerable.Range(1, int.MaxValue).Except(_items.Keys).First(), item);
         }
+
+        /// <summary>
+        /// Removes the item at the index
+        /// </summary>
+        /// <param name="key">Index</param>
+        public void Remove(int key)
+        {
+            _items.Remove(key);
+        }
+
+        /// <summary>
+        /// Removes the matching item from the list if it could be found
+        /// </summary>
+        /// <param name="item"></param>
+        public void Remove(T item)
+        {
+            KeyValuePair<int, T> entry = _items.FirstOrDefault(i => i.Value.Equals(item));
+
+            if (entry.Value == null)
+            {
+                return;
+            }
+
+            Remove(entry.Key);
+        }
+
+        /// <summary>
+        /// Serializes the object to json and returns the json as string
+        /// </summary>
+        /// <returns></returns>
+        public string GetSerialized() => JsonConvert.SerializeObject(this, Formatting.Indented);
+
+        /// <summary>
+        /// Creates a SimpleCollection object with the given json string
+        /// </summary>
+        /// <param name="json">Object as json string</param>
+        /// <returns></returns>
+        public static SimpleCollection<T> GetDeserialized(string json) => JsonConvert.DeserializeObject<SimpleCollection<T>>(json);
 
         public IEnumerator<T> GetEnumerator()
         {
@@ -38,9 +91,5 @@ namespace Kinobuchungssystem
         {
             return Items.GetEnumerator();
         }
-
-        public string GetSerialized() => JsonConvert.SerializeObject(this);
-
-        public static SimpleCollection<T> GetDeserialized(string json) => JsonConvert.DeserializeObject<SimpleCollection<T>>(json);
     }
 }
