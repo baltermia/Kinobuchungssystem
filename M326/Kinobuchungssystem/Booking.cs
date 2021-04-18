32f1b79ce9beeefe9971a8e498a392162f450074
speyck <54800864+speyck@users.noreply.github.com>
2021-04-18 10:45:51 +0200

    feat: Added GetPanel and GetNewFromPanel to Booking class

--- M326/Kinobuchungssystem/Booking.cs
+++ M326/Kinobuchungssystem/Booking.cs
@@ -1,7 +1,9 @@
-﻿namespace Kinobuchungssystem
+﻿using System.Windows;
+using System.Windows.Controls;
 
+namespace Kinobuchungssystem
 {
-    public class Booking : ICinemaType
+    public class Booking
     {
         public readonly Show Show;
 
@@ -16,6 +18,46 @@
         public override string ToString()
         {
             return Customer?.Firstname + " " + Customer?.Lastname + ": " + Show?.ToString();
+        }
+
+        public static StackPanel GetPanel(Cinema cinema)
+        {
+            StackPanel panel = new StackPanel();
+
+            TextBlock tbkShow = new TextBlock()
+            {
+                Text = "Vorführung",
+                FontWeight = FontWeights.Bold
+            };
+            ComboBox cbxShow = new ComboBox()
+            {
+                ItemsSource = cinema.Shows
+            };
+
+            TextBlock tbkCustomer = new TextBlock()
+            {
+                Text = "Kunde",
+                FontWeight = FontWeights.Bold
+            };
+            ComboBox cbxCustomer = new ComboBox()
+            {
+                ItemsSource = cinema.Customers
+            };
+
+            panel.Children.Add(tbkShow);
+            panel.Children.Add(cbxShow);
+            panel.Children.Add(tbkCustomer);
+            panel.Children.Add(cbxCustomer);
+
+            return panel;
+        }
+
+        public static Booking GetNewFromPanel(StackPanel panel)
+        {
+            Show show = (Show)((ComboBox)panel.Children[1]).SelectedItem;
+            Customer customer = (Customer)((ComboBox)panel.Children[3]).SelectedItem;
+
+            return new Booking(show, customer);
         }
     }
 }
