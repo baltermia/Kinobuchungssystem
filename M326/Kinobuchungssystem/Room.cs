f6c19dc83b0415faed8c35064b8d0e70424ca970
speyck <54800864+speyck@users.noreply.github.com>
2021-04-18 10:45:18 +0200

    feat: Added GetPanel and GetNewFromPanel to Room class

--- M326/Kinobuchungssystem/Room.cs
+++ M326/Kinobuchungssystem/Room.cs
@@ -1,6 +1,10 @@
-﻿namespace Kinobuchungssystem
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Kinobuchungssystem
 {
-    public class Room : ICinemaType
+    public class Room
     {
 	    public readonly string Name;
 		
@@ -10,6 +14,41 @@
         {
             Name = name;
             Seats = seats;
+        }
+
+        public static StackPanel GetPanel()
+        {
+            StackPanel panel = new StackPanel();
+
+            TextBlock tbkName = new TextBlock()
+            {
+                Text = "Name",
+                FontWeight = FontWeights.Bold
+            };
+            TextBox tbxName = new TextBox();
+
+            TextBlock tbkSeats = new TextBlock()
+            {
+                Text = "Sitzplätze",
+                FontWeight = FontWeights.Bold,
+                Margin = new Thickness(0, 10, 0, 0)
+            };
+            TextBox tbxSeats = new TextBox();
+
+            panel.Children.Add(tbkName);
+            panel.Children.Add(tbxName);
+            panel.Children.Add(tbkSeats);
+            panel.Children.Add(tbxSeats);
+
+            return panel;
+        }
+
+        public static Room GetNewFromGrid(Panel panel)
+        {
+            string name = ((TextBox)panel.Children[1]).Text;
+            int seats = Convert.ToInt32(((TextBox)panel.Children[3]).Text);
+
+            return new Room(name, seats);
         }
 
         public override string ToString()
