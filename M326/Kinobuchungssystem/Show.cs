6eba0fc2e6605b2d83ebba63297c12857455d4bf
speyck <54800864+speyck@users.noreply.github.com>
2021-04-18 10:45:11 +0200

    feat: Added GetPanel and GetNewFromPanel to Show class

--- M326/Kinobuchungssystem/Show.cs
+++ M326/Kinobuchungssystem/Show.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+using Xceed.Wpf.Toolkit;
 
 namespace Kinobuchungssystem
 {
-    public class Show : ICinemaType
+    public class Show
     {
         public readonly Room Room;
 
@@ -23,6 +26,66 @@ namespace Kinobuchungssystem
         public override string ToString()
         {
             return Movie?.Title + " in " + Room?.Name + " am " + Start.ToString("dd.MM.yyyy") + " von " + Start.ToString("HH:mm") + "-" + End.ToString("HH:mm");
+        }
+
+        public static StackPanel GetPanel(Cinema cinema)
+        {
+            StackPanel panel = new StackPanel();
+
+            TextBlock tbkRoom = new TextBlock()
+            {
+                Text = "Saal",
+                FontWeight = FontWeights.Bold
+            };
+            ComboBox cbxRoom = new ComboBox()
+            {
+                ItemsSource = cinema.Rooms
+            };
+
+            TextBlock tbkMovie= new TextBlock()
+            {
+                Text = "Film",
+                FontWeight = FontWeights.Bold
+            };
+            ComboBox cbxMovie = new ComboBox()
+            {
+                ItemsSource = cinema.Movies
+            };
+
+            TextBlock tbkStart = new TextBlock()
+            {
+                Text = "Start",
+                FontWeight = FontWeights.Bold
+            };
+            DateTimePicker dpkStart = new DateTimePicker();
+
+            TextBlock tbkEnd = new TextBlock()
+            {
+                Text = "Ende",
+                FontWeight = FontWeights.Bold
+            };
+            DateTimePicker dpkEnd = new DateTimePicker();
+
+            panel.Children.Add(tbkRoom);
+            panel.Children.Add(cbxRoom);
+            panel.Children.Add(tbkMovie);
+            panel.Children.Add(cbxMovie);
+            panel.Children.Add(tbkStart);
+            panel.Children.Add(dpkStart);
+            panel.Children.Add(tbkEnd);
+            panel.Children.Add(dpkEnd);
+
+            return panel;
+        }
+
+        public static Show GetNewFromPanel(StackPanel panel)
+        {
+            Room room = (Room)((ComboBox)panel.Children[1]).SelectedItem;
+            Movie movie = (Movie)((ComboBox)panel.Children[3]).SelectedItem;
+            DateTime start = ((DatePicker)panel.Children[5]).SelectedDate ?? DateTime.MinValue;
+            DateTime end = ((DatePicker)panel.Children[7]).SelectedDate ?? DateTime.MaxValue;
+
+            return new Show(room, movie, start, end);
         }
     }
 }
